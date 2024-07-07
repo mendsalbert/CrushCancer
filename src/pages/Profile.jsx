bee2a5af0852a2c13b5c5959f2c4f12cc3f50332
mendsalbert <mendsalbert@gmail.com>
2024-07-07 22:01:20 +0000

    refactor(profile): remove unused fetchCampaigns functionality
    Deleted the unused `fetchCampaigns`, `isLoading`, and `campaigns` state logic in Profile page. The `getUserCampaigns` function from `useStateContext` was also no longer being used.
    
    This streamlines the Profile component by removing unnecessary commented-out code, making the file cleaner and easier to comprehend. This change should not affect the functionality as this code wasn't being used. Future updates can reintroduce similar logic when and if necessary.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,18 +4,6 @@ import { DisplayCampaigns } from "../components";
 // import { useStateContext } from '../context'
 
 const Profile = () => {
-  // const [isLoading, setIsLoading] = useState(false);
-  // const [campaigns, setCampaigns] = useState([]);
-
-  // const { address, contract, getUserCampaigns } = useStateContext();
-
-  // const fetchCampaigns = async () => {
-  //   setIsLoading(true);
-  //   const data = await getUserCampaigns();
-  //   setCampaigns(data);
-  //   setIsLoading(false);
-  // }
-
   // useEffect(() => {
   //   if(contract) fetchCampaigns();
   // }, [address, contract]);
