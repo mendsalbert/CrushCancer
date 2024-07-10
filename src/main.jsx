8fe504b95fa517a0f369edd7eee76a7a1a7d0835
mendsalbert <mendsalbert@gmail.com>
2024-07-10 16:23:54 +0000

    feat: switch to theta testnet for authentication
    Configures the app to use theta testnet instead of the previously employedsetId as the authentication provider. The switch was made to the theta testnet for more robust testing and the appId was adjusted accordingly.
    
    Commented out blocks of code in `App.jsx` that depend on the authentication status since they're not currently needed in this transition phase.
    
    Also, the logo configuration was removed from the appearance configuration. If branding is deemed necessary later, it can be re-introduced.
    
    Please note the change in the appId that requires continuous monitoring.
    
    This change could affect how the users authenticate, therefore, the user experience may change.
    
    Relevant issues: #123

--- src/main.jsx
+++ src/main.jsx
@@ -9,17 +9,41 @@ import { PrivyProvider } from "@privy-io/react-auth";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+export const thetaTestnet = {
+  id: 365,
+  name: "Theta ",
+  network: "Theta Testnet",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Theta Testnet",
+    symbol: "TFUEL",
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://eth-rpc-api-testnet.thetatoken.org/rpc"],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: "Theta Explorer",
+      url: "https://testnet-explorer.thetatoken.org/",
+    },
+  },
+  testnet: true,
+};
+
 root.render(
   <PrivyProvider
-    appId="clxt5gya607hj5gfcjmhvi6iw"
+    appId="clyg131vo07o513s6lb580wfy"
     config={{
       appearance: {
         theme: "dark",
-        logo: "https://i.ibb.co/NntLZ1h/heart-handshake.png",
       },
       embeddedWallets: {
         createOnLogin: "users-without-wallets",
       },
+      defaultChain: thetaTestnet,
+      supportedChains: [thetaTestnet],
     }}
   >
     <Router>
