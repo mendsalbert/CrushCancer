210467c5d1c1a884a1516f296908792eb5ab648b
mendsalbert <mendsalbert@gmail.com>
2024-07-12 21:23:24 +0000

    refactor(context): clean up unnecessary imports from context index
    Removed unused import statements from the context's index file to enhance readability and maintainability. This change eliminates clutter and complications caused by having non-functional, commented-out code, thereby promoting cleaner and more accessible code architecture. No associated issues.

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -1,11 +1,4 @@
 import React, { useContext, createContext } from "react";
-
-// import {
-//   useAddress,
-//   useContract,
-//   useMetamask,
-//   useContractWrite,
-// } from "@thirdweb-dev/react";
 import { ethers } from "ethers";
 // import { EditionMetadataWithOwnerOutputSchema } from "@thirdweb-dev/sdk";
 
