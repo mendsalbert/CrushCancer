9ae3649685107281e5e2711af54e0de46c77132c
mendsalbert <mendsalbert@gmail.com>
2024-07-15 17:26:40 +0000

    refactor: remove unused donation function
    Unused donation function has been removed from the StateContextProvider. This improves readability and maintainability, as the codebase no longer includes dead code. While the function was likely once useful for managing donations, it appears to be completely unused at present. Future implementations requiring similar functionality should reconsider the needs of the application and develop a fresh implementation for best fit. No functionality has been affected by this cleanup.
    
    Refs: #596b00e to #207092a

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -67,10 +67,6 @@ export const StateContextProvider = ({ children }) => {
   // const donate = async (pId, amount) => {
   //   // const data = await contract.call("donateToCampaign", [pId], {
   //   //   value: ethers.utils.parseEther(amount),
-  //   // });
-
-  //   // return data;
-  // };
 
   return <StateContext.Provider>{children}</StateContext.Provider>;
 };
